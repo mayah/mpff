65f8ee52cb90f2d6c40350cc4d71ee6ca43dfe2d
MAYAH <mayah@mayah.jp>
2013-10-26 22:04:41 +0900

    Adds setSessionValue to MPFFActionContext.

--- app/mpff/controllers/MPFFActionContext.scala
+++ app/mpff/controllers/MPFFActionContext.scala
@@ -6,13 +6,24 @@ import play.api.mvc.Request
 class MPFFActionContext(val request: Request[AnyContent],
                         val sessionToken: String,
                         val sessionId: String) {
+  // ----------------------------------------------------------------------
+  // Session related methods
   var sessionValues: List[(String, String)] = List.empty
   def addSessionValue(key: String, value: String) {
     sessionValues = (key, value) :: sessionValues
   }
+
+  // Removes all sessionValues having |key|, and adds (key, value).
+  def setSessionValue(key: String, value: String) {
+    sessionValues = (key, value) :: sessionValues.filter { case (k, v) => k != key }
+  }
+
   def discardSession() {
     sessionValues = List.empty
   }
+
+  // ----------------------------------------------------------------------
+  // LongLive Session related methods. Actually this is a cookie.
 
   var longliveSessionValues: List[(String, String)] = List.empty
   def addLongLiveSessionValue(key: String, value: String) {
@@ -21,6 +32,9 @@ class MPFFActionContext(val request: Request[AnyContent],
   def discardLongLiveSession() {
     longliveSessionValues = List.empty
   }
+
+  // ----------------------------------------------------------------------
+  // Misc.
 
   var flashingValues: List[(String, String)] = List.empty
   def addFlashing(key: String, value: String) {
